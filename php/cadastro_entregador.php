f6b843e521acfc6bbfa0d889cbdeaecfd519005f
JoaoP7 <78989620+JoaoP7@users.noreply.github.com>
2024-11-11 23:42:54 -0300

    Update cadastro_entregador.php
    Criando codigo v1

--- php/cadastro_entregador.php
+++ php/cadastro_entregador.php
@@ -4,6 +4,19 @@ $servername = "127.0.0.1";
 $username = "root"; 
 $password = "Root"; 
 $dbname = "cadastro_entregadores"; 
+// Criação da conexão
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+// Verificando a conexão
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Recebendo os dados do formulário
+$nome = isset($_POST['nome']) ? $_POST['nome'] : '';
+$sobrenome = $_POST['sobrenome'];
+$imagem = isset($_POST['imagem']) ? $_POST['imagem'] : '';  
+$dia = $_POST['dia'];
+$mes = $_POST['mes'];
 
 ?>
