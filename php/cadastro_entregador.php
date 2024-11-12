95adf46b67ff998f687072b6ed721ddd95f4fb93
JoaoP7 <78989620+JoaoP7@users.noreply.github.com>
2024-11-11 23:43:43 -0300

    Update cadastro_entregador.php
    Criação v2

--- php/cadastro_entregador.php
+++ php/cadastro_entregador.php
@@ -18,5 +18,16 @@ $sobrenome = $_POST['sobrenome'];
 $imagem = isset($_POST['imagem']) ? $_POST['imagem'] : '';  
 $dia = $_POST['dia'];
 $mes = $_POST['mes'];
-
+$ano = $_POST['ano'];
+$rg = $_POST['rg'];
+$cpf = $_POST['cpf'] . $_POST['cpf2'];
+$rua = $_POST['rua'];
+$numero = $_POST['numero'];
+$bairro = $_POST['bairro'];
+$estado = $_POST['estado'];
+$cidade = $_POST['cidade'];
+$cep = $_POST['cep'] . $_POST['cep2'];
+$veiculo =  isset($_POST['veiculo']) ? $_POST['veiculo'] : '' ;
+$placa =  isset($_POST['placa']) ? $_POST['placa'] : '';
+$ano_veiculo = isset($_POST['ano_veiculo']) ? $_POST['ano_veiculo'] : '2000';
 ?>
