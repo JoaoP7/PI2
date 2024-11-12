fe8312d133aa4bde46320a5f8d144f3da98d0851
JoaoP7 <78989620+JoaoP7@users.noreply.github.com>
2024-11-11 23:46:41 -0300

    Update Script_do_Formulario.php.php
    v1

--- php/Script_do_Formulario.php.php
+++ php/Script_do_Formulario.php.php
@@ -16,5 +16,15 @@ if ($conn->connect_error) {
 $nome = $_POST['nome'];
 $sobrenome = $_POST['sobrenome'];
 $nascimento = $_POST['ano'] . '-' . $_POST['mes'] . '-' . $_POST['dia'];
-
+ $rg = $_POST['rg'];
+$cpf = $_POST['cpf'] . $_POST['cpf2'];
+$rua = $_POST['rua'];
+$numero = $_POST['numero'];
+$bairro = $_POST['bairro'];
+$estado = $_POST['estado'];
+$cidade = $_POST['cidade'];
+$cep = $_POST['cep'] . $_POST['cep2'];
+$veiculo = $_POST['veiculo'];
+$placa = $_POST['placa'];
+$ano_veiculo = $_POST['ano_veiculo'];
 ?>
