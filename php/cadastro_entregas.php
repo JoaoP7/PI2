426e820c8733f9f329a308f98d22e47e474c6f57
JoaoP7 <78989620+JoaoP7@users.noreply.github.com>
2024-11-11 23:45:07 -0300

    Update cadastro_entregas.php
    Criação v1

--- php/cadastro_entregas.php
+++ php/cadastro_entregas.php
@@ -13,4 +13,24 @@ if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
 
+
+// Verificando se o formulário foi enviado
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Pegando os dados do formulário
+    $nome_cliente = $_POST['email'];
+    $numero_pedido = $_POST['sobrenome'];
+    $data_entrega = $_POST['dia'] . '-' . $_POST['mes'] . '-' . $_POST['ano'];
+    $hora = $_POST['rg'];
+    $valor = $_POST['valor'];
+    $entregador = $_POST['entregador'];
+    $veiculo = $_POST['email'];
+    $placa = $_POST['login'];
+    $ano_veiculo = $_POST['Modelo'];
+    $modelo_veiculo = $_POST['Modelo'];
+
+    // Preparando a consulta SQL para inserção
+    $sql = "INSERT INTO entregas (nome_cliente, numero_pedido, data_entrega, hora, valor, entregador, veiculo, placa, ano_veiculo, modelo_veiculo)
+            VALUES ('$nome_cliente', '$numero_pedido', '$data_entrega', '$hora', '$valor', '$entregador', '$veiculo', '$placa', '$ano_veiculo', '$modelo_veiculo')";
+
+    
 ?>
